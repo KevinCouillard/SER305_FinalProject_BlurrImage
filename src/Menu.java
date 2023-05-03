902ad4ae5169c408c54edb18ffbbcb0e45042c68
AlexSanteramo <santeramoalex01@gmail.com>
2023-05-03 11:26:32 -0400

    Upload button working

--- src/Menu.java
+++ src/Menu.java
@@ -1,20 +1,22 @@
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JToolBar;
+import boofcv.gui.image.ImagePanel;
+
+import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 
 public class Menu extends JPanel{
-    
-    public Menu(){
+    UploadButton upload;
+    JButton download;
+    ImagePanel gui;
+    public Menu(ImagePanel image){
         super(new BorderLayout());
-        
+        gui = image;
         JToolBar toolbar = new JToolBar();
-        JButton upload = new JButton("Upload");
-        JButton download = new JButton("Download");
+        upload = new UploadButton("Upload", this, gui);
+        download = new JButton("Download");
         toolbar.add(upload);
         toolbar.add(download);
         add(toolbar, BorderLayout.PAGE_START);
         
     }
-
 }
---
+++ src/UploadButton.java
@@ -0,0 +1,53 @@
+import boofcv.gui.image.ImagePanel;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class UploadButton extends JButton {
+    private Menu _menu;
+    private ImagePanel panel;
+    public UploadButton(String text, Menu menu, ImagePanel gui) {
+        super(text);
+        _menu = menu;
+        panel = gui;
+        this.addActionListener(new UploadButtonListener());
+        this.setActionCommand(text);
+    }
+
+    private class UploadButtonListener implements ActionListener
+    {
+        //If the button is clicked it terminates the program.
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            // TODO Auto-generated method stub
+            if(e.getActionCommand().equals("Upload"))
+            {
+                JFileChooser upload = new JFileChooser();
+                upload.setCurrentDirectory(new File("."));
+                int res = upload.showOpenDialog(null);
+
+                if(res == JFileChooser.APPROVE_OPTION) {
+                    File path = new File(upload.getSelectedFile().getAbsolutePath());
+                    //BufferedImage img = new BufferedImage(400,400,BufferedImage.TYPE_4BYTE_ABGR);
+                    try {
+                        BufferedImage img = ImageIO.read(path);
+                        if(panel == null) {
+                            System.out.println("Panel null");
+                        }
+                        panel.setImage(img);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    System.out.println(path);
+                }
+            }
+        }
+
+    }
+}
